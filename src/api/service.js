885fa7de576b5393d75bd6b7d35f4379aa32b3d3
ArtYu12 <korzaevmatvej@gmail.com>
2024-05-30 14:13:26 +0600

    first commit

--- src/api/service.js
+++ src/api/service.js
@@ -1,6 +1,6 @@
 import toast from "react-hot-toast"
 
-export const baseURL = "http://localhost:5000"
+export const baseURL = " https://592f-5-251-226-161.ngrok-free.app"
 const token = "cXVlcnlfaWQ9QUFGRUR6Uk5BQUFBQUVRUE5FMVlJQjMzJnVzZXI9JTdCJTIyaWQlMjIlM0ExMjk1MjU3NDEyJTJDJTIyZmlyc3RfbmFtZSUyMiUzQSUyMkRhbmlpbCUyMiUyQyUyMmxhc3RfbmFtZSUyMiUzQSUyMiUyMiUyQyUyMnVzZXJuYW1lJTIyJTNBJTIyZDlBMm5ZM0ElMjIlMkMlMjJsYW5ndWFnZV9jb2RlJTIyJTNBJTIycnUlMjIlMkMlMjJpc19wcmVtaXVtJTIyJTNBdHJ1ZSUyQyUyMmFsbG93c193cml0ZV90b19wbSUyMiUzQXRydWUlN0QmYXV0aF9kYXRlPTE3MTU1MDMxMDImaGFzaD01OGQ2YWMxMTNhNjQwYzFiNDAzNzhkZTFiOGFhNDU1YTRjNDExYmEwYjM1YjA2Y2FmMWQwMmFkMTNjYjc3Yzcy"
 //window.btoa(window.Telegram.WebApp.initData)
 const appAPI = {
